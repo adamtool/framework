3b2e82ca681184d42216df9f8b50009a29c30f16
Manuel Gieseking <mgieseking@users.noreply.github.com>
2018-10-27 18:41:39 +0200

    MG: added the possibility to put all the external path to the ADAM.properties file and can use it from ever u want. If u want to used it in an ide please put -DPROPERTY_FILE=<mainfolder>/ADAM.properties as parameter to the run call.

--- tools/src/uniolunisaar/adam/tools/AdamProperties.java
+++ tools/src/uniolunisaar/adam/tools/AdamProperties.java
@@ -1,5 +1,8 @@
 package uniolunisaar.adam.tools;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.Properties;
 
@@ -7,38 +10,41 @@ public class AdamProperties {
 
     private static AdamProperties instance = null;
     private static Properties props;
-    private static final String LIBFOLDER = "libfolder";
+    private static final String PROPERTY_FILE = "PROPERTY_FILE";
+    public static final String LIBRARY_FOLDER = "libraryFolder";
+    public static final String AIGER_TOOLS = "aigertools";
+    public static final String MC_HYPER = "mcHyper";
+    public static final String ABC = "abcBin";
 
-    private AdamProperties() {//throws ClassNotFoundException, IOException {
-        props = new Properties();
-//        InputStream input = null;
-//        String filename = "ADAM.prop";
-//        input = ADAMProperties.class.getClassLoader().getResourceAsStream(filename);
-//        if (input == null) {
-//            throw new ClassNotFoundException(filename);
-//        }
-//
-//        //load a properties file from class path, inside static method
-//        props.load(input);
-//
-//        input.close();
-        String folder = System.getProperty(LIBFOLDER);
-        props.setProperty(LIBFOLDER, folder == null ? "./lib" : folder);
+    private AdamProperties() {
+        // Load the property file which location is given as parameter of the console
+        String file = System.getProperty(PROPERTY_FILE);
+        InputStream in = null;
+        try {
+            if (file == null) {
+                throw new RuntimeException("Couldn't find the property file of ADAM. It has not specified as parameter of the java call of ADAM.");
+            }
+            props = new Properties();
+            in = new FileInputStream(file);
+            props.load(in);
+        } catch (IOException ex) {
+            throw new RuntimeException("Couldn't read the property file of ADAM: " + file);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("Couldn't close the property file of ADAM: " + file);
+                }
+            }
+        }
     }
 
-    public static AdamProperties getInstance() {//throws ClassNotFoundException, IOException {
+    public static AdamProperties getInstance() {
         if (instance == null) {
             instance = new AdamProperties();
         }
         return instance;
-    }
-    
-    public String getLibFolder() {
-        return props.getProperty(LIBFOLDER);
-    }
-
-    public void setLibFolder(String folder) {
-        props.setProperty(LIBFOLDER, folder);
     }
 
     public synchronized Object setProperty(String key, String value) {
