ce13da11e541da67b16cc1bd71ab71a4c4bb00b1
Manuel Gieseking <mgieseking@users.noreply.github.com>
2018-12-14 00:38:13 +0100

    MG: restructuring of the model checking part, to have a common module handeling the transformation for the model checking and the monolithic synthesis.

---
+++ petrinetWithTransits/src/uniolunisaar/adam/logic/transformers/pn2aiger/Circuit.java
@@ -0,0 +1,32 @@
+package uniolunisaar.adam.logic.transformers.pn2aiger;
+
+/**
+ *
+ * @author Manuel Gieseking
+ */
+public class Circuit {
+
+    public enum Renderer {
+        INGOING,
+        INGOING_REGISTER,
+        OUTGOING,
+        OUTGOING_REGISTER,
+        OUTGOING_REGISTER_MAX_INTERLEAVING;
+    }
+
+    public static AigerRenderer getRenderer(Renderer renderer) {
+        switch (renderer) {
+            case INGOING:
+                return new AigerRendererSafeIn();
+            case INGOING_REGISTER:
+                throw new RuntimeException("Not yet implemented.");
+            case OUTGOING:
+                return new AigerRendererSafeOut();
+            case OUTGOING_REGISTER:
+                return new AigerRendererSafeOutStutterRegister();
+            case OUTGOING_REGISTER_MAX_INTERLEAVING:
+                return new AigerRendererSafeOutStutterRegisterMaxInterleaving();
+        }
+        throw new RuntimeException("The case " + renderer + " is not yet implemented.");
+    }
+}
