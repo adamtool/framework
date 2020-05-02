fcfc861b4bde2eb8a1586150dde977082eccf4ff
Manuel Gieseking <mgieseking@users.noreply.github.com>
2020-05-02 13:45:59 +0200

    MG: added the dual until operator to CTL

--- petrinetWithTransits/src/uniolunisaar/adam/ds/abta/TreeState.java
+++ petrinetWithTransits/src/uniolunisaar/adam/ds/abta/TreeState.java
@@ -1,12 +1,15 @@
 package uniolunisaar.adam.ds.abta;
 
 import uniolunisaar.adam.ds.automata.BuchiState;
+import uniolunisaar.adam.ds.automata.ILabeled;
 
 /**
  *
  * @author Manuel Gieseking
  */
-public class TreeState extends BuchiState {
+public class TreeState extends BuchiState implements ILabeled {
+
+    private String label = "";
 
     TreeState(String id) {
         super(id);
@@ -15,6 +18,15 @@ public class TreeState extends BuchiState {
     @Override
     protected void setBuchi(boolean buchi) {
         super.setBuchi(buchi);
+    }
+
+    @Override
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
     }
 
 }
